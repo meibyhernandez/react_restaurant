42c2aa4a3eafe64fe46df7c1afc2a6ab4556dcc9
Meiby Hernández Morales <meahernandezmo@unal.edu.co>
2022-11-14 15:48:55 +0100

    menu items data mapping

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -6,34 +6,38 @@ import {images, data} from '../../constants';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div className='app__specialMenu flex__center section__padding' id='menu'>
-    <div className='app__specialMenu-title'>
-      <SubHeading title="Menu that fits you palette"/>
-      <h1 className='headtext_cormorant'>Today's Special</h1>
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits your palatte" />
+      <h1 className="headtext__cormorant">Today&apos;s Special</h1>
     </div>
-    <div className='app_specialMenu-menu'>
-      <div className='app__specialMenu-menu_wine flex__center'>
-        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
-        <div className='app__specialMenu_menu_items'>
-          {data.wines.map((wine, index)=>(
-            <p>{wine.title}</p>
+
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine  flex__center">
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu_menu_items">
+          {data.wines.map((wine, index) => (
+            <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
           ))}
         </div>
       </div>
-      <div className='app__specialMenu-menu_img'>
-        <img src={images.menu} alt="menu img"/>
+
+      <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="menu__img" />
       </div>
-      <div className='app__specialMenu-menu_cocktails flex__center'>
-        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
-        <div className='app__specialMenu_menu_items'>
-          {data.cocktails.map((cocktail, index)=>(
-            <p>{cocktail.title}</p>
+
+      <div className="app__specialMenu-menu_cocktails  flex__center">
+        <p className="app__specialMenu-menu_heading">Cocktails</p>
+        <div className="app__specialMenu_menu_items">
+          {data.cocktails.map((cocktail, index) => (
+            <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags} />
           ))}
         </div>
       </div>
     </div>
-    <div style={{marginTop: '15px'}}>
-      <button type='button' className='custom__button'>View More</button>
+
+    <div style={{ marginTop: 15 }}>
+      <button type="button" className="custom__button">View More</button>
     </div>
   </div>
 );
